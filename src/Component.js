4bc099b1fdc9ab605d0fe3177107ef77b8861e0e
mairalala <mairaylala2000@gmail.com>
2024-05-18 12:52:56 -0500

    add events to display the text in the input and update button

--- src/Component.js
+++ src/Component.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Componet() {
+
+  const [text, setText ] = useState()
+  const [updated, setUpdated ] = useState()
+
+  const textOnChange = (event) => {
+    setText(event.target.value)
+  }
+
+  const buttonOnClick = () => {
+    setUpdated(text)
+  }
+
   return (
-    <div>My Componet</div>
+    <div>
+      <input type="text" vale={text} onChange={textOnChange}/>
+      <button onClick={buttonOnClick}>Actualizar</button>
+      <p>Texto input: {text} </p>
+      <p>Texto actualizado: {updated}</p>
+      
+    </div>
   )
 }
